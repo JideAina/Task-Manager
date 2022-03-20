b5c35c18fc255ae780879d983bd449a254045bd7
Mathew Olajide <moolajide@mail.usf.edu>
2022-03-19 21:58:33 -0400

    task5 code

--- js/index.js
+++ js/index.js
@@ -1,31 +1,33 @@
-const validFormFieldInput = (event) => {
-    event.preventDefault();
-    const nameInput = document.querySelector('#name');
-    const name = nameInput.value;
-    const descriptionInput = document.querySelector('#description');
-    const description = descriptionInput.value;
-    const assignedInput = document.querySelector('#assigned');
-    const assigned = assignedInput.value;
-    const dueDateInput = document.querySelector('#due_date');
-    const dueDate = dueDateInput.value;
-    const statusInput = document.querySelector('#status');
-    const status = statusInput.value;
-    const alert = document.querySelector('#alert');
-    if (!name || !description || !assigned || !dueDate || !status) {
+const nameInput = document.querySelector('#name');
+// const name = nameInput.value;
+const descriptionInput = document.querySelector('#description');
+// const description = descriptionInput.value;
+const assignedInput = document.querySelector('#assigned');
+// const assigned = assignedInput.value;
+const dueDateInput = document.querySelector('#due_date');
+// const dueDate = dueDateInput.value;
+const statusInput = document.querySelector('#status');
+// const status = statusInput.value;
+const alert = document.querySelector('#alert');
+const newTaskForm = document.getElementById('form');
+
+
+const validFormFieldInput = () => {
+    if (!nameInput.value || !descriptionInput.value || !assignedInput.value || !dueDateInput.value || !statusInput.value) {
         let errorList = [];
-        if (!name) {
+        if (!nameInput.value) {
             errorList.push(" name");
         }
-        if (!description) {
+        if (!descriptionInput.value) {
             errorList.push(" description");
         }
-        if (!assigned) {
+        if (!assignedInput.value) {
             errorList.push(" assigned to");
         }
-        if (!dueDate) {
+        if (!dueDateInput.value) {
             errorList.push(" due date");
         }
-        if (!status) {
+        if (!statusInput.value) {
             errorList.push(" status");
         }
         alert.classList.add("invalid-form-alert");
@@ -41,9 +43,29 @@ const validFormFieldInput = (event) => {
         if (!alert.classList.contains("hide-alert")) {
             alert.classList.add("hide-alert");
         }
-        console.log(`name: ${name} description: ${description} assigned: ${assigned} due date: ${dueDate} status: ${status}`);
+        console.log(`name: ${nameInput.value} description: ${descriptionInput.value} assigned: ${assignedInput.value} due date: ${dueDateInput.value} status: ${statusInput.value}`);
+        return true;
     }
-}
 
-const taskManager = new TaskManager()
-console.log(taskManager.tasks);
+
+}
+const taskManager = new TaskManager();
+
+newTaskForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    let isFormValidation = validFormFieldInput();
+    
+    if(isFormValidation){
+        
+        taskManager.addTask(nameInput.value,descriptionInput.value,assignedInput.value,dueDateInput.value,statusInput.value);
+        console.log(taskManager.tasks);
+        nameInput.value = '';
+        descriptionInput.value = '';
+        assignedInput.value = '';
+        dueDateInput.value = '';
+        statusInput.value = '';
+    }
+    
+});
+
+// console.log(createTaskHtml('Take out trash','Take out trash in the bedrooms','Mathew','Today','done'))
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,6 +1,57 @@
+const createTaskHtml = (name,description,assigned,date,status) => {
+    const html = `
+    <li class="list-group-item">
+        <div class="d-flex w-100 mt-2 justify-content-between align-items-center">
+            <h5>${name}</h5>
+            <span class="badge badge-danger">${status}</span>
+        </div>
+        <div class="d-flex w-100 mb-3 justify-content-between">
+            <small>Assigned To: ${assigned}</small>
+            <small>Due: ${date}</small>
+        </div>
+        <p>${description}</p>
+    </li>
+    `
+    return html;
+}
+
+
+
+
+
+
 class TaskManager {
-    constructor() {
+    constructor(currentId = 0) {
         this.tasks = [];
+        // this.currentId = 0;
+        this.currentId = currentId;
     }
     
+    addTask(name,description,assigned,date,status = "TODO"){
+        this.currentId++;
+        const newTask = {
+            id : this.currentId,
+            name: name,
+            description : description,
+            assignedTo : assigned,
+            date: date,
+            status: status
+        }
+
+        this.tasks.push(newTask);
+    }
+
+    render(){
+        let tasksHtmlList = [];
+        for(let i = 0; i < this.tasks.length; i++){
+            let currentTask = this.task[i];
+            let currentDate = new Date(currentTask[date]);
+            let formattedDate = currentDate.toDateString();
+            let taskHtml = createTaskHtml(currentTask.name,currentTask.description,currentTask.assigned,formattedDate,currentTask.status);
+
+            tasksHtmlList.push(taskHtml)
+
+
+        }
+    }
 }
